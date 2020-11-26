a26eb1cf2fa5d9f98a8f5f98d5e43a2857e080ec
Srilaxsmi <66507421+Srilaxsmi@users.noreply.github.com>
2020-11-26 14:56:30 +0530

    Update prob3.py

--- prob3.py
+++ prob3.py
@@ -1,9 +1,8 @@
-N = 600851475143
-for i in range(2,N):
-    while N % i == 0:
-        N = N/i
-    if i > N:
-        print(i)
-        break
+def lpf(N):
+    for i in range(2,N):
+        while N % i == 0:
+            N = N/i
+        if i > N:
+            return i
 
-
+print(lpf(600851475143))
